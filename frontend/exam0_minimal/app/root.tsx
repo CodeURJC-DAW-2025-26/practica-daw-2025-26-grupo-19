08b604ecdd68b8e1f7c172801d27271b941c25c3
v1ctor2911 <omar29112004@gmail.com>
2026-04-27 22:23:51 +0200

    footer mini fix

--- frontend/exam0_minimal/app/root.tsx
+++ frontend/exam0_minimal/app/root.tsx
@@ -18,8 +18,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <Meta />
                 <Links />
-            </head>
-            <body>
+            </head>            
+            <body className="flex flex-col min-h-screen">
                 {children}
                 <ScrollRestoration />
                 <Scripts />
